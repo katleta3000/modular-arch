5314a8016c1f99f825cd1215da0d92b6ef1c7fee
Evgenii Rtishchev <e.rtishchev@gmail.com>
2025-06-04 14:01:10 +0100

    Initial commit; Setup configs

---
+++ Applications/AppB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppB
+//
+//  Created by Evgenii Rtischev on 04/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
